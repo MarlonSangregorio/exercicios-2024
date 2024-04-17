c56ed1166cf03c8d60c5287c65b0038dcdf82b1b
marlon <marlonsousa0321@gmail.com>
2024-04-17 00:12:24 -0300

    montando componente list

--- dart/lib/main.dart
+++ dart/lib/main.dart
@@ -14,7 +14,7 @@ void main() {
 class ChuvaDart extends StatelessWidget {
   const ChuvaDart({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the rohjjyhot of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
